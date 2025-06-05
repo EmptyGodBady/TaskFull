5bfea0ead391d0e0a0b0cd248a31c7cfcf804ec6
Vitalii <virtusprodam12@gmail.com>
2025-06-05 14:10:42 +0200

    feat/8:add description to cards

---
+++ components/modals/card-modal/actions.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Actions() {
+  return (
+    <div>
+      <p>actions</p>
+    </div>
+  );
+}
+
+Actions.Skeleton = function ActionsSkeleton() {
+  return (
+    <div className="space-y-2 mt-2">
+      <Skeleton className="w-20 h-4 bg-neutral-200" />
+      <Skeleton className="w-full h-8 bg-neutral-200" />
+      <Skeleton className="w-full h-8 bg-neutral-200" />
+    </div>
+  );
+};
--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -2,20 +2,44 @@
 
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { useCardModal } from "@/hooks/use-card-modal";
+import { fetcher } from "@/lib/fetcher";
+import { CardWithList } from "@/types";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { useQuery } from "@tanstack/react-query";
+import Header from "./header";
+import Description from "./description";
+import Actions from "./actions";
+
 export default function CardModal() {
   const id = useCardModal((state) => state.id);
   const isOpen = useCardModal((state) => state.isOpen);
   const onClose = useCardModal((state) => state.onClose);
 
-  if (!id) return null;
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <VisuallyHidden>
         <DialogTitle>Hidden Dialog Title</DialogTitle>
       </VisuallyHidden>
-      <DialogContent>i am modal</DialogContent>
+      <DialogContent>
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
+            </div>
+          </div>
+          {!cardData ? <Actions.Skeleton/> : }
+        </div>
+      </DialogContent>
     </Dialog>
   );
 }
